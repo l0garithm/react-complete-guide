126e00509a39187e3154d11de86ce2d512190292
Logan - Sanctuary <lacarlson5@gmail.com>
2021-11-22 05:54:29 +0000

    Lessons 30-34

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Sucka ma dicka
       </header>
+      <body>
+        <ExpenseItem />
+        <ExpenseItem />
+        <ExpenseItem />
+      </body>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div>March 28, 2021</div>
+            <div>
+                <h2>Car Insurance</h2>
+            </div>
+            <div>
+                $249.67
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
